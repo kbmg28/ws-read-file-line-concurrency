0a220e04f7f4c3ddf1cac76c29175d794e70d65f
Kevin Gomes <kb.developer.br@gmail.com>
2023-11-28 11:31:53 +0000

    initial commit

---
+++ src/main/java/br/com/kbmg/wsreadfilelineconcurrency/WsReadFileLineConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package br.com.kbmg.wsreadfilelineconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsReadFileLineConcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsReadFileLineConcurrencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kbmg/wsreadfilelineconcurrency/WsReadFileLineConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kbmg.wsreadfilelineconcurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsReadFileLineConcurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
